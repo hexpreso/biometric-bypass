16af710964082c997d55f8d1e4da3dafda5b6732
Rafa <32096750+rafareborn@users.noreply.github.com>
2025-04-18 22:11:19 +0200

    chore(signing): switch release buildType to use release signing config

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,7 +41,7 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
-            signingConfig = signingConfigs["debug"]
+            signingConfig = signingConfigs.getByName("release")
         }
         debug {
             isMinifyEnabled = false

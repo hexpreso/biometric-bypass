269f163f77fd8616b018ac06cc431b69f82cde9b
Rafa <rafareborn@users.noreply.github.com>
2025-04-24 20:09:58 +0200

    release: bump version to 1.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         minSdk = 29
         targetSdk = 35
 
-        versionCode = 100
-        versionName = "1.0.0"
+        versionCode = 101
+        versionName = "1.0.1"
     }
 
     signingConfigs {
